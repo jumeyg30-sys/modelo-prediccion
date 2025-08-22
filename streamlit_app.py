c96225591779eb94c4ced5cd351c5888196be312
jumeyg30-sys <jumeyg30@gmail.com>
2025-08-22 13:14:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('Visualización de resultados del Modelo Predictio')
+st.title('🐦 Visualización de Resultados del Modelo Predictio')
 
 st.write('Modelo multivariante para predecir abundancia y diversidad de aves según variables climáticas en el campus de la ESPOL ')
