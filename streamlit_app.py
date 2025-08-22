5d496639872a1c1cad489d220a83cace7fab2bb3
jumeyg30-sys <jumeyg30@gmail.com>
2025-08-22 14:00:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,22 @@
 import streamlit as st
+import zipfile
+import os
 import pandas as pd
 
 st.title('🐦 Visualización de Resultados del Modelo Predictivo')
 
 st.info('Modelo multivariante para predecir abundancia y diversidad de aves según variables climáticas en el campus de la ESPOL ')
 
-# ZIP
-unzip out.zip
-zip
+# 1. Ruta al archivo comprimido (ajusta según tu estructura de carpetas)
+zip_path = os.path.join("data", "out.zip")
 
+# 2. Extraer dentro de la carpeta data/
+with zipfile.ZipFile(zip_path, "r") as zip_ref:
+    zip_ref.extractall("data")
+
+# 3. Ahora leer el CSV extraído
+csv_path = os.path.join("data", "out.csv")
+df_out = pd.read_csv(csv_path)
+
+# Para verificar
+print(df_out.head())
